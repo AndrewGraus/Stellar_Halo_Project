d805238ba1b8914ef4385d72a1e7bc80770a4a64
Andrew Graus <agraus@utexas.edu>
2020-09-15 15:32:37 -0700

    initial attempt to apply regressor to data

---
+++ Regressor_apply_to_data.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import h5py
+
+f_halo = h5py.File('../m12i_res_7100_cdm/halo/halo_600.hdf5')
+
+pos_halo = f_halo['position'][:]
+mass_halo = f_halo['mass'][:]
+radius_halo = f_halo['radius'][:]
+vel_halo = f_halo['velocity'][:]
+
+f_predict = h5py.File('./halo_1107_Z13_particles.hdf5')
+
+h = 0.675
+
+coord_predict = f_predict['PartType1']['Coordinates'][:]
+vel_predict = f_predict['PartType1']['Velocities'][:]
+id_predict = f_predict['PartType1']['ParticleIDs'][:]
+
+host_cen = np.array([37.53820323, 34.52403051, 37.02327211])*1000.0
+host_vel = np.array([ 57.161152, -83.112122, -17.277088])
+
+coord_diff_predict = coord_predict - host_cen
+vel_diff_predict = vel_predict - host_vel
+
+print('running Vector regressor')
+
+phase_space_coords =  np.concatenate((coord_diff_predict,vel_diff_predict),axis=1)
+
+model = tf.keras.models.load_model('./saved_models/Classifier_biased_sigmoid.h5')
+
+model_output = model.predict(phase_space_coords)
+
+f_write = h5py.File('./predictions_from_regressor.hdf5')
+f_write.create_dataset("PartType1/ParticleIDs",data=id_predict)
+f_write.create_dataset("PartType1/mass_ratio",data=model_output)
+
+
+print('finished')
